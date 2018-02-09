0d57a579b4479c4e889cfbdca483b884c6b6280d
Gabriel Machado <g_machado1@hotmail.com>
2018-02-09 18:58:22 -0200

    Adicionado efeito de transição na troca de paginas

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,9 @@
       </ul>
     </nav>
 
-    <router-view></router-view>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -30,5 +32,13 @@ export default {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
+  }
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .3s
+  }
+  
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
   }
 </style>
