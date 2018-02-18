d56afa8b49fc960f0ba4f52647c602b8ca3b467b
Gabriel Machado <g_machado1@hotmail.com>
2018-02-18 11:07:02 -0300

    Feito alteração para permitir lazy loading e code split no build

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import Home from './components/home/Home.vue';
-import Cadastro from './components/cadastro/Cadastro.vue';
+const Cadastro = () => System.import('./components/cadastro/Cadastro.vue').then(m => m.default);
 
 export const routes = [
     { path: '', name: 'home', component: Home, titulo: 'Home', menu: true },
