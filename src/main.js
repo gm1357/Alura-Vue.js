93b7c19e803375261eed8cac1d93f0b1775ebe71
Gabriel Machado <g_machado1@hotmail.com>
2018-02-18 10:23:50 -0300

    Importado VeeValidate

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
+import VeeValidate from 'vee-validate'
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000';
@@ -12,6 +13,8 @@ const router = new VueRouter({
   routes,
   mode: 'history'
 });
+
+Vue.use(VeeValidate);
 
 new Vue({
   el: '#app',
